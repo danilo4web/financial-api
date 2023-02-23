05ec4be3732dc2eaceb2651c11929bc8f53c32de
danilo <danilo4web@gmail.com>
2023-02-23 20:01:41 -0300

    migrations with account, customer and transactions table

---
+++ database/migrations/2023_02_23_225459_create_accounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('balance', 8, 2)->default(0);
+            $table->string('account_number')->unique();
+            $table->unsignedBigInteger('customer_id');
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
